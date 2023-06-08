46afd79ce25f07c0ea326f47cb41d3f23511ab40
mvishok <stum011852@kvschennairegion.in>
2023-06-08 10:53:44 +0530

    Added SimpleUpdater

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import quote
 from ctypes import windll
 from os.path import dirname, abspath
-import updater
+from requests import get
 
-resp = updater.get("https://api.quotable.io/random").json()
+from SimpleUpdater import SimpleUpdater
+
+CURRENT_VERSION = '0.0.1'
+VERSION_URL = 'https://raw.githubusercontent.com/mvishok/InspireWall/main/ver.txt'
+SimpleUpdater.checkForUpdates(CURRENT_VERSION, VERSION_URL)
+
+resp = get("https://api.quotable.io/random").json()
 content = resp['content'] + " - " + resp['author']
 quote.save(content, 612, 612)
 p = dirname(abspath(__file__))+'\quote.png'
